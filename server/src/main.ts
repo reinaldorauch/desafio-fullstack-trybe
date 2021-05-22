144e731ee05d18dfcf7c7cab93627bf38efa504c
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2021-05-22 03:27:52 -0300

    Atualizado versão do angular

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,19 +4,27 @@ import { AppModule } from './app.module';
 import { ValidationError } from 'class-validator';
 import { LoginDataDto } from './login-data.dto';
 import { NotFoundExceptionFilter } from './not-found-exception.filter';
-import fs from 'fs';
+import * as fs from 'fs';
+const { access, copyFile } = fs.promises;
 
 async function bootstrap() {
-	// Jeito simples de inicializar o currencies.json
-	await initCurrencies();
-	const app = await NestFactory.create(AppModule);
-	app.setGlobalPrefix('api');
-	app.useGlobalFilters(new NotFoundExceptionFilter());
-	app.enableCors();
-	await app.listen(3000);
+  // Jeito simples de inicializar o currencies.json
+  await initCurrencies();
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new NotFoundExceptionFilter());
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
 
 async function initCurrencies() {
-	fs.access();
+  const { W_OK, R_OK, F_OK } = fs.constants;
+  const RW_ACCESS = W_OK | R_OK | F_OK;
+  const fn = process.cwd() + '/currencies.json';
+  try {
+    await access(fn, RW_ACCESS);
+  } catch (err) {
+    await copyFile(fn + '.init', fn);
+  }
 }
